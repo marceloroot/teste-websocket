08e03c8df420e89d1b4fea8de59bbd05b74be9a0
marceloroot <marcelo.lima.gomes.23@gmail.com>
2024-09-10 17:56:21 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,144 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
 
-export default function Home() {
+const socket = io("http://localhost:3333");
+
+const App: React.FC = () => {
+  const [transcription, setTranscription] = useState<string>("");
+  const [symptoms, setSymptoms] = useState<string>("");
+  const [isRecording, setIsRecording] = useState<boolean>(false); // Estado para controlar o status da gravação
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const audioChunksRef = useRef<Blob[]>([]);
+  const streamRef = useRef<MediaStream | null>(null);
+  const recordingIntervalRef = useRef<number | null>(null);
+
+  const sendAudio = async () => {
+    if (audioChunksRef.current.length > 0) {
+      const audioBlob = new Blob(audioChunksRef.current, { type: "audio/wav" });
+      const audioArrayBuffer = await audioBlob.arrayBuffer();
+      console.log("ArrayBuffer size:", audioArrayBuffer.byteLength);
+      audioChunksRef.current = []; // Limpar os chunks de áudio
+
+      socket.emit("audio-stream", {
+        audio: audioArrayBuffer,
+        currentText: transcription, // Enviando a transcrição atual junto com o áudio
+      });
+    }
+  };
+
+  const lookForSymptoms = async (text: string) => {
+    socket.emit("look-for-symptoms", {
+      text,
+    });
+  };
+
+  const startRecording = async () => {
+    setTranscription(""); // Limpar a transcrição
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    streamRef.current = stream;
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorderRef.current = mediaRecorder;
+
+    mediaRecorder.ondataavailable = (event: BlobEvent) => {
+      audioChunksRef.current.push(event.data);
+    };
+
+    mediaRecorder.onstart = () => {
+      console.log("Recording started");
+      setIsRecording(true); // Atualizar estado para gravando
+    };
+
+    mediaRecorder.onstop = () => {
+      if (recordingIntervalRef.current) {
+        clearInterval(recordingIntervalRef.current);
+      }
+      sendAudio(); // Enviar quaisquer dados de áudio restantes
+      setIsRecording(false); // Atualizar estado para não gravando
+    };
+
+    mediaRecorder.start(); // Iniciar gravação
+  };
+
+  const stopRecording = () => {
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+    }
+    if (streamRef.current) {
+      streamRef.current.getTracks().forEach((track) => track.stop());
+    }
+  };
+
+  useEffect(() => {
+    // Listener para transcrição
+    socket.on("transcription-result", (transcription: string) => {
+      console.log("Received transcription:", transcription);
+      setTranscription(transcription);
+      lookForSymptoms(transcription); // Analisar sintomas na transcrição recebida
+    });
+
+    // Listener para erros de transcrição
+    socket.on("transcription-error", (error: string) => {
+      setTranscription(`Error: ${error}`);
+    });
+
+    // Listener para sintomas identificados
+    socket.on("symptoms-result", (data: { Symptoms: string[] }) => {
+      if (data && data.Symptoms) {
+        console.log("Symptoms result:", data.Symptoms);
+        setSymptoms(JSON.stringify(data.Symptoms));
+      }
+    });
+
+    return () => {
+      socket.off("transcription-result");
+      socket.off("transcription-error");
+      socket.off("symptoms-result");
+    };
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div>
+      <h1>Real-Time Audio Transcription</h1>
+      <button
+        onClick={startRecording}
+        style={{
+          padding: "1rem",
+          backgroundColor: isRecording ? "#ccc" : "#4CAF50", // Verde quando não estiver gravando
+          color: isRecording ? "#000" : "#fff", // Texto branco quando não está gravando
+          cursor: isRecording ? "not-allowed" : "pointer", // Desativado enquanto grava
+          border: "none",
+          borderRadius: "5px",
+          marginRight: "10px",
+        }}
+        disabled={isRecording} // Desabilitar enquanto estiver gravando
+      >
+        Start Recording
+      </button>
+      <button
+        onClick={stopRecording}
+        style={{
+          padding: "1rem",
+          backgroundColor: isRecording ? "#f44336" : "#ccc", // Vermelho quando estiver gravando
+          color: "#fff",
+          cursor: !isRecording ? "not-allowed" : "pointer", // Desativado se não estiver gravando
+          border: "none",
+          borderRadius: "5px",
+        }}
+        disabled={!isRecording} // Desabilitar se não estiver gravando
+      >
+        Stop Recording
+      </button>
+      <div id="transcription">
+        <h3>Transcription:</h3>
+        <p>{transcription}</p>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div id="symptoms">
+        <h3>Symptoms Detected:</h3>
+        <p>{symptoms}</p>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default App;
